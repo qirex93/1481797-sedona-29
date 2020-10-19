cb9ccedf55645fafa3023874c66b8b0a6091714d
qirex93 <60400506+qirex93@users.noreply.github.com>
2020-10-19 04:28:36 +0300

    испарвления по замечаниям

--- js/modal.js
+++ js/modal.js
@@ -3,74 +3,77 @@ const searchPopup = document.querySelector(".modal-hotels");
 const inputs = document.querySelectorAll(".modal-hotels input");
 const buttons = document.querySelectorAll(".modal-hotels button");
 
+const adultsPlusButton = searchPopup.querySelector(".adults-container .cross");
+const adultsCountInput = searchPopup.querySelector(".adults");
+const childrenMinusButton = searchPopup.querySelector(".children-container .minus");
+const adultsMinusButton = searchPopup.querySelector(".adults-container .minus");
+const childrenPlusButton = searchPopup.querySelector(".children-container .cross");
+const childrenCountInput = searchPopup.querySelector(".children");
 
-/* open-close modal-window */
+
+var initsScript = function () {
+  searchPopup.classList.add("modal-off");
+};
+try {
+  window.addEventListener("load", initsScript , false);
+} catch(e) {
+  window.onload = initsScript;
+}
+
 
 searchButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    if (searchPopup.classList.contains("modal-show-down")) {
-        searchPopup.classList.add("modal-show-up");
-        searchPopup.classList.remove("modal-show-down");
-        inputs.forEach(function(x) {
-            x.setAttribute("disabled", "disabled");
-        })
-        buttons.forEach(function(x) {
-            x.setAttribute("disabled", "disabled");
-        })
-    }
+  evt.preventDefault();
+  if (searchPopup.classList.contains("modal-show-down")) {
+      searchPopup.classList.remove("modal-off");
+      searchPopup.classList.add("modal-show-up");
+      searchPopup.classList.remove("modal-show-down");
+      inputs.forEach(function (item) {
+          item.setAttribute("disabled", "disabled");
+      })
+      buttons.forEach(function (item) {
+        item.setAttribute("disabled", "disabled");
+      })
+}
 
-    else {
-        searchPopup.classList.remove("modal-show-up");
-        searchPopup.classList.add("modal-show-down");
-        inputs.forEach(function(x) {
-            x.removeAttribute("disabled");
-        })
-        buttons.forEach(function(x) {
-            x.removeAttribute("disabled");
-        })
-    }
+  else {
+    searchPopup.classList.remove("modal-off");
+    searchPopup.classList.remove("modal-show-up");
+    searchPopup.classList.add("modal-show-down");
+    inputs.forEach(function (item) {
+      item.removeAttribute("disabled");
+      })
+    buttons.forEach(function (item) {
+      item.removeAttribute("disabled");
+    })
+  }
 });
 
 
 /* plus count button*/
 
-const  adultsPlusButton = searchPopup.querySelector(".adults-container .cross");
-const  adultsCountInput = searchPopup.querySelector(".adults");
-
 adultsPlusButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    const volNumber = adultsCountInput.getAttribute("value")
-    adultsCountInput.setAttribute("value", parseInt(volNumber)+1)
+  evt.preventDefault();
+  const volNumber = adultsCountInput.getAttribute("value")
+  adultsCountInput.setAttribute("value", parseInt(volNumber) + 1)
 })
 
-const  childrenPlusButton = searchPopup.querySelector(".children-container .cross");
-const  childrenCountInput = searchPopup.querySelector(".children");
-
 childrenPlusButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    const volNumber = childrenCountInput.getAttribute("value")
-    childrenCountInput.setAttribute("value", parseInt(volNumber)+1)
+  evt.preventDefault();
+  const volNumber = childrenCountInput.getAttribute("value")
+  childrenCountInput.setAttribute("value", parseInt(volNumber) + 1)
 })
 
 
 /* minus count button*/
 
-const  adultsMinusButton = searchPopup.querySelector(".adults-container .minus");
-
 adultsMinusButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    const volNumber = adultsCountInput.getAttribute("value")
-    adultsCountInput.setAttribute("value", parseInt(volNumber)-1)
+  evt.preventDefault();
+  const volNumber = adultsCountInput.getAttribute("value")
+  adultsCountInput.setAttribute("value", parseInt(volNumber) - 1)
 })
-
-const  childrenMinusButton = searchPopup.querySelector(".children-container .minus");
 
 childrenMinusButton.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    const volNumber = childrenCountInput.getAttribute("value")
-    childrenCountInput.setAttribute("value", parseInt(volNumber)-1)
+  evt.preventDefault();
+  const volNumber = childrenCountInput.getAttribute("value")
+  childrenCountInput.setAttribute("value", parseInt(volNumber) - 1)
 })
-
-
-
-
