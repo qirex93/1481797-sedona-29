da9e41fabb17c294601a30c15b23c38c01eb92b8
qirex93 <60400506+qirex93@users.noreply.github.com>
2020-10-19 21:17:05 +0300

    оживлены ползунки в фильтре (но требуют доработок)

--- js/priceSlider.js
+++ js/priceSlider.js
@@ -1,12 +1,9 @@
 
-//* work in progress
-
-/*document.onmousedown = function(e) {			//событие нажатия на кнопку мыши
+document.onmousedown = function(e) {			//событие нажатия на кнопку мыши
        
   const minToggle = document.querySelector(".toggle-min"); 	
   const maxToggle = document.querySelector(".toggle-max");
   const rangeControls = document.querySelector(".range-controls"); 
- 
   var elem = e.target; 							//элемент, на который нажали
   
   if (!elem.classList.contains('toggle')) return; 
@@ -20,75 +17,33 @@
     left: rangeControls.clientLeft
   };
 
- //* переделка 
-  var targetCoords = rangeControls.getBoundingClientRect();
-  var newLocation = {
-    x: limits.left
-  };
-  if (e.offsetX > limits.right) {
-    newLocation.x = limits.right;
-  } else if (e.offsetX > limits.left) {
-    newLocation.x = e.clientX - targetCoords.left;
-  }
-  relocate(newLocation);
-
-
-//* размещение small
-function relocate(newLocation) {
-  elem.style.left = newLocation.x + 'px';
-}
-
-} 
-*/
-
-
-var small = document.querySelector(".toggle-min");
-var big = document.querySelector(".range-controls"); 
-//* флаг перетаскивания
-var isDrag = false;
-//* ограничения, за которые нельзя вытащить small
-var limits = {
-  right: big.clientWidth + big.clientLeft - small.clientWidth,
-  left: big.clientLeft
-};
-
-//* вкл/выкл режим перетаскивания
-small.onmousedown = function(e) {
-  isDrag = true;
-}
-document.onmouseup = function() {
-  isDrag = false;
-}
-document.onmousemove = function(e) {
-  if (isDrag) {
-    move(e);
-  }
-}
-
-//* вычисление координат
-function move(e) {
-  var targetCoords = big.getBoundingClientRect();
-  var newLocation = {
-    x: limits.left,
-  };
-  if (e.offsetX > limits.right) {
-    newLocation.x = limits.right;
-  } else if (e.offsetX > limits.left) {
-    newLocation.x = e.clientX - targetCoords.left;
-  }
-  relocate(newLocation);
-  console.log(e.offsetX);
-  console.log(limits.right);
-  console.log(newLocation.x);
-}
-
-//* размещение small
-function relocate(newLocation) {
-  small.style.left = newLocation.x + 'px';
-}
-
-
  
+  rangeControls.onmousemove = function(e) {
+      move(e);
+  }
 
+  function move(e) {
+    var targetCoords = rangeControls.getBoundingClientRect();
+    var newLocation = {
+      x: limits.left,
+    };
+    if (e.offsetX > limits.right) {
+      newLocation.x = limits.right;
+    } else if (e.offsetX >= limits.left) {
+      newLocation.x = e.clientX - targetCoords.left;
+    } 
+    relocate(newLocation);
+  }
+  
+  //* размещение small
+  function relocate(newLocation) {
+    elem.style.left = newLocation.x + 'px';
+  }
+
+  elem.onmouseup = function() {
+    rangeControls.onmousemove = null;
+    elem.onmouseup = null;
+  };
+} 
 
 
