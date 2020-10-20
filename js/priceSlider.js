b47386b46c02f86d60e5f049c3e4ea1ec95b3481
qirex93 <60400506+qirex93@users.noreply.github.com>
2020-10-20 12:36:32 +0300

    правки по замечаниям наставника

--- js/priceSlider.js
+++ js/priceSlider.js
@@ -1,23 +1,17 @@
-
+//* work in progress
 document.onmousedown = function(e) {			//событие нажатия на кнопку мыши
-       
-  const minToggle = document.querySelector(".toggle-min"); 	
-  const maxToggle = document.querySelector(".toggle-max");
+  
   const rangeControls = document.querySelector(".range-controls"); 
   var elem = e.target; 							//элемент, на который нажали
-  
-  if (!elem.classList.contains('toggle')) return; 
-  
-  elem.ondragstart = function() {   				// отменяем Drag’n’Drop браузера
-    return false
-  };   
-
   var limits = {
     right: rangeControls.clientWidth + rangeControls.clientLeft - elem.clientWidth,
     left: rangeControls.clientLeft
   };
 
- 
+  elem.ondragstart = function() {   				// отменяем Drag’n’Drop браузера
+    return false
+  };   
+
   rangeControls.onmousemove = function(e) {
       move(e);
   }
@@ -27,6 +21,7 @@ document.onmousedown = function(e) {			//событие нажатия на кн
     var newLocation = {
       x: limits.left,
     };
+    
     if (e.offsetX > limits.right) {
       newLocation.x = limits.right;
     } else if (e.offsetX >= limits.left) {
@@ -38,12 +33,18 @@ document.onmousedown = function(e) {			//событие нажатия на кн
   //* размещение small
   function relocate(newLocation) {
     elem.style.left = newLocation.x + 'px';
-  }
+    elem.onmouseup = function() {
+      rangeControls.onmousemove = null;
+      elem.onmouseup = null;
+    };
+  
+    console.log("offset " + e.offsetX);
+    console.log("limits " + limits.right);
+    console.log("limits " + limits.left);
+    console.log("location " + newLocation.x);
+  } 
 
-  elem.onmouseup = function() {
-    rangeControls.onmousemove = null;
-    elem.onmouseup = null;
-  };
+ 
 } 
 
 
